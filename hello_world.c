af5bf5a971db94b7e87fc2fe40a76c915b7c9cef
linzhehuang <linzhehuang@outlook.com>
2017-05-04 22:56:39 +0800

    The templatet of comment

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+/**
+ * File: hello_world.c
+ * Description: 一个简单的c语言程序
+ * Author: linzhehuang
+ * Blog: http://www.cnblogs.com/linzhehuang/
+ * Version: 0.0.1
+ * History: 
+ *   2017.5.4 创建该文件
+ */
+#include <stdio.h>
+int main(void){
+	printf("Hello world!");
+	return 0;
+}
